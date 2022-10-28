0a7372a71628f75c500d96300455602afb557bde
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2022-10-28 16:42:32 +0200

    creata base griglia

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,26 @@ const griglia = document.getElementById('griglia');
 console.log(griglia);
 
 
+function createSquare(){
+    const div = document.createElement('div');
+    div.classList.add('square');
+    return div;
+}
+
+console.log(createSquare());
+
+
+
+for(let i = 0; i<64; i++){
+    let currentElement = createSquare();
+    console.log(currentElement);
+
+    currentElement.addEventListener('click', function(){
+        console.log(this);
+        this.classList.toggle('active');
+    })
+
+    griglia.append(currentElement);
+
+}
 
