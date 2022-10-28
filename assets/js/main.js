d3ce4c300b4e4783570406aeb15d6e78deb629c7
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2022-10-28 17:21:31 +0200

    tentativi con if

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,46 @@
-const griglia = document.getElementById('griglia');
-console.log(griglia);
+let level = document.getElementById("level").value;
+
+function play() {
+    console.log('play');
+
+  if (level == "easy") {
 
 
-function createSquare(){
-    const div = document.createElement('div');
-    div.classList.add('square');
-    return div;
+
+    console.log('easy');
+    
+    console.log("play2");
+    const griglia = document.getElementById("griglia");
+    console.log(griglia);
+
+    function createSquare() {
+      const div = document.createElement("div");
+      div.classList.add("square");
+      return div;
+    }
+
+    console.log(createSquare());
+
+    for (let i = 0; i < 100; i++) {
+      let currentElement = createSquare();
+      console.log(currentElement);
+
+      currentElement.addEventListener("click", function () {
+        // console.log(this);
+        this.classList.toggle("active");
+      });
+
+      griglia.append(currentElement);
+    }
+
+
+
+
+  } else if (level == "medium") {
+    console.log('medium');
+  } else if (level == "hard"){
+    console.log('hard');
+  } else {
+    console.log('uccidimi')
+  }
 }
-
-console.log(createSquare());
-
-
-
-for(let i = 0; i<64; i++){
-    let currentElement = createSquare();
-    console.log(currentElement);
-
-    currentElement.addEventListener('click', function(){
-        console.log(this);
-        this.classList.toggle('active');
-    })
-
-    griglia.append(currentElement);
-
-}
-
